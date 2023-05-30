aac19bb0722cee9d8dea9894c7ee0c2651bb57d4
s5hy25ni <s5hy25ni@gmail.com>
2023-05-30 17:21:59 +0900

    알고리즘 문제

---
+++ src/com/sh/al/Algorithm001.java
@@ -0,0 +1,13 @@
+package com.sh.al;
+
+import java.util.Arrays;
+
+public class Algorithm001 {
+
+	public int sum(int n) {
+		int sum = 0;
+		
+		return sum;
+
+	}
+}
---
+++ src/com/sh/al/AlgorithmMain.java
@@ -0,0 +1,12 @@
+package com.sh.al;
+
+public class AlgorithmMain {
+
+	public static void main(String[] args) {
+		Algorithm001 a1 = new Algorithm001();
+		int result = a1.sum(12345);
+		System.out.println(result);
+
+	}
+
+}
