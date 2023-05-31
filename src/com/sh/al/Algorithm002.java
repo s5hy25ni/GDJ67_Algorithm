5a18e3a34c1df14bc5984123c4f845ee29b7ce32
s5hy25ni <s5hy25ni@gmail.com>
2023-05-31 17:47:50 +0900

    2번 완료 & 생각 더 해보기

--- src/com/sh/al/Algorithm002.java
+++ src/com/sh/al/Algorithm002.java
@@ -8,24 +8,7 @@ import java.util.List;
 public class Algorithm002 {
 	public boolean sumCheck(int[] arr, int n) {
 		boolean isc = false;
-		Arrays.sort(arr);
 		
-//		List<Integer> arrList = new ArrayList<Integer>(Arrays.asList(arr));
-		List<Integer> lists = new ArrayList<Integer>();
-		for (int i = 0; i < arr.length; i++) {
-			if(arr[i]>=n) {
-				break;
-			}
-			lists.add(arr[i]);
-		}
-		
-		Iterator<Integer> iter = lists.iterator();
-		while(iter.hasNext()) {
-			if(lists.contains(n-iter.next())) {
-				isc = true;
-				break;
-			}
-		}
 		
 		return isc;
 	}
