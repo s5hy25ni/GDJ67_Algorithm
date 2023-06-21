4a194f60b8e2493ffc2bf0f4c35576848f9bb9c0
s5hy25ni <s5hy25ni@gmail.com>
2023-06-21 20:50:58 +0900

    2023-06-21

--- src/com/sh/al/AlgorithmMain.java
+++ src/com/sh/al/AlgorithmMain.java
@@ -1,5 +1,7 @@
 package com.sh.al;
 
+
+
 public class AlgorithmMain {
 
 	public static void main(String[] args) {
@@ -7,24 +9,24 @@ public class AlgorithmMain {
 //		int result = a1.solution(513138484);
 //		System.out.println(result);
 
-		Algorithm002 a2 = new Algorithm002();
+//		Algorithm002 a2 = new Algorithm002();
 //		boolean isc1 = a2.solutionStream(new int[] {5,3,9,13}, 8);
 //		boolean isc2 = a2.solutionStream(new int[] {5,3,9,13}, 7);
 //		boolean isc3 = a2.solutionJCF(new int[] {5,3,9,13}, 8);
 //		boolean isc4 = a2.solutionJCF(new int[] {5,3,9,13}, 7);
-		boolean isc5 = a2.solution(new int[] {5,3,9,13}, 8);
-		boolean isc6 = a2.solution(new int[] {5,3,9,13}, 7);
-		boolean isc7 = a2.solution(new int[] {5,3,9,13}, 22);
-		boolean isc8 = a2.solution(new int[] {5,3,9,13}, 15);
+//		boolean isc5 = a2.solution(new int[] {5,3,9,13}, 8);
+//		boolean isc6 = a2.solution(new int[] {5,3,9,13}, 7);
+//		boolean isc7 = a2.solution(new int[] {5,3,9,13}, 22);
+//		boolean isc8 = a2.solution(new int[] {5,3,9,13}, 15);
 		
 //		System.out.println(isc1);
 //		System.out.println(isc2);
 //		System.out.println(isc3);
 //		System.out.println(isc4);
-		System.out.println(isc5);
-		System.out.println(isc6);
-		System.out.println(isc7);
-		System.out.println(isc8);
+//		System.out.println(isc5);
+//		System.out.println(isc6);
+//		System.out.println(isc7);
+//		System.out.println(isc8);
 		
 //		Algorithm003 a3 = new Algorithm003();
 //		int cycle1 = a3.solution("aaaaaaa");
@@ -34,8 +36,11 @@ public class AlgorithmMain {
 //		System.out.println(cycle2);
 //		System.out.println(cycle3);
 		
-		Algorithm004 a4 = new Algorithm004();
-		a4.solution(new int[] {3,5,2,1,2,6}, new int[] {2,3,4,6,3,2});
+//		Algorithm004 a4 = new Algorithm004();
+//		a4.solution(new int[] {3,5,2,1,2,6}, new int[] {2,3,4,6,3,2});
+
+	 
+	        // SHA-256 메시지 다이제스트 인쇄
 
 	}
 
